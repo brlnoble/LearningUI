3a2774437097b8dcd7a201f3d1cfb41b2c48766e
brlnoble <51976754+brlnoble@users.noreply.github.com>
2019-12-31 16:22:27 -0500

    Update MyForm.h

--- MyForm.h
+++ MyForm.h
@@ -1,4 +1,4 @@
-#pragma once
+	#pragma once
 #include<random>
 #include<time.h>
 
@@ -45,7 +45,7 @@ namespace RandomNumberGenerator {
 	private: System::Windows::Forms::Label^  label7;
 	private: System::Windows::Forms::Button^  button1;
 	private: System::Windows::Forms::Button^  button2;
-	private: System::Windows::Forms::PictureBox^  pictureBox1;
+
 	protected:
 
 	private:
@@ -59,9 +59,13 @@ namespace RandomNumberGenerator {
 		/// Required method for Designer support - do not modify
 		/// the contents of this method with the code editor.
 		/// </summary>
+
+		//Initialize variables for the layout of buttons
+		int height = 250;
+		int length = 900;
+
 		void InitializeComponent(void)
 		{
-			System::ComponentModel::ComponentResourceManager^  resources = (gcnew System::ComponentModel::ComponentResourceManager(MyForm::typeid));
 			this->label1 = (gcnew System::Windows::Forms::Label());
 			this->label2 = (gcnew System::Windows::Forms::Label());
 			this->label3 = (gcnew System::Windows::Forms::Label());
@@ -71,76 +75,74 @@ namespace RandomNumberGenerator {
 			this->label7 = (gcnew System::Windows::Forms::Label());
 			this->button1 = (gcnew System::Windows::Forms::Button());
 			this->button2 = (gcnew System::Windows::Forms::Button());
-			this->pictureBox1 = (gcnew System::Windows::Forms::PictureBox());
-			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox1))->BeginInit();
 			this->SuspendLayout();
 			// 
 			// label1
 			// 
 			this->label1->AutoSize = true;
-			this->label1->Location = System::Drawing::Point(171, 60);
+			this->label1->Location = System::Drawing::Point(((length)/14)*1, height/4);
 			this->label1->Name = L"label1";
-			this->label1->Size = System::Drawing::Size(46, 17);
+			this->label1->Size = System::Drawing::Size(16, 17);
 			this->label1->TabIndex = 0;
 			this->label1->Text = L"0";
 			// 
 			// label2
 			// 
 			this->label2->AutoSize = true;
-			this->label2->Location = System::Drawing::Point(271, 60);
+			this->label2->Location = System::Drawing::Point(((length)/14)*3, height / 4);
 			this->label2->Name = L"label2";
-			this->label2->Size = System::Drawing::Size(46, 17);
+			this->label2->Size = System::Drawing::Size(16, 17);
 			this->label2->TabIndex = 1;
 			this->label2->Text = L"0";
 			// 
 			// label3
 			// 
 			this->label3->AutoSize = true;
-			this->label3->Location = System::Drawing::Point(366, 60);
+			this->label3->Location = System::Drawing::Point(((length)/14)*5, height / 4);
 			this->label3->Name = L"label3";
-			this->label3->Size = System::Drawing::Size(46, 17);
+			this->label3->Size = System::Drawing::Size(16, 17);
 			this->label3->TabIndex = 2;
 			this->label3->Text = L"0";
 			// 
 			// label4
 			// 
 			this->label4->AutoSize = true;
-			this->label4->Location = System::Drawing::Point(464, 60);
+			this->label4->Location = System::Drawing::Point(((length)/14)*7, height / 4);
 			this->label4->Name = L"label4";
-			this->label4->Size = System::Drawing::Size(46, 17);
+			this->label4->Size = System::Drawing::Size(16, 17);
 			this->label4->TabIndex = 3;
 			this->label4->Text = L"0";
 			// 
 			// label5
 			// 
 			this->label5->AutoSize = true;
-			this->label5->Location = System::Drawing::Point(567, 60);
+			this->label5->Location = System::Drawing::Point(((length)/14)*9, height / 4);
 			this->label5->Name = L"label5";
-			this->label5->Size = System::Drawing::Size(46, 17);
+			this->label5->Size = System::Drawing::Size(16, 17);
 			this->label5->TabIndex = 4;
 			this->label5->Text = L"0";
 			// 
 			// label6
 			// 
 			this->label6->AutoSize = true;
-			this->label6->Location = System::Drawing::Point(669, 60);
+			this->label6->Location = System::Drawing::Point(((length)/14)*11, height / 4);
 			this->label6->Name = L"label6";
-			this->label6->Size = System::Drawing::Size(46, 17);
+			this->label6->Size = System::Drawing::Size(16, 17);
 			this->label6->TabIndex = 5;
 			this->label6->Text = L"0";
 			// 
 			// label7
 			// 
 			this->label7->AutoSize = true;
-			this->label7->Location = System::Drawing::Point(783, 60);
+			this->label7->Location = System::Drawing::Point(((length)/14)*13, height / 4);
 			this->label7->Name = L"label7";
-			this->label7->Size = System::Drawing::Size(46, 17);
+			this->label7->Size = System::Drawing::Size(16, 17);
 			this->label7->TabIndex = 6;
 			this->label7->Text = L"0";
 			// 
-			// button1, Generate
+			// button1
 			// 
-			this->button1->Location = System::Drawing::Point(231, 139);
+			this->button1->Location = System::Drawing::Point((length/8)*3, 2*height / 4);
 			this->button1->Name = L"button1";
 			this->button1->Size = System::Drawing::Size(100, 30);
 			this->button1->TabIndex = 7;
@@ -148,9 +150,9 @@ namespace RandomNumberGenerator {
 			this->button1->UseVisualStyleBackColor = true;
 			this->button1->Click += gcnew System::EventHandler(this, &MyForm::button1_Click);
 			// 
-			// button2, Reset
+			// button2
 			// 
-			this->button2->Location = System::Drawing::Point(697, 139);
+			this->button2->Location = System::Drawing::Point((length/8)*4, 2*height / 4);
 			this->button2->Name = L"button2";
 			this->button2->Size = System::Drawing::Size(100, 30);
 			this->button2->TabIndex = 8;
@@ -158,21 +160,11 @@ namespace RandomNumberGenerator {
 			this->button2->UseVisualStyleBackColor = true;
 			this->button2->Click += gcnew System::EventHandler(this, &MyForm::button2_Click);
 			// 
-			// pictureBox1
-			// 
-			this->pictureBox1->Image = (cli::safe_cast<System::Drawing::Image^>(resources->GetObject(L"pictureBox1.Image")));
-			this->pictureBox1->Location = System::Drawing::Point(21, 40);
-			this->pictureBox1->Name = L"pictureBox1";
-			this->pictureBox1->Size = System::Drawing::Size(126, 122);
-			this->pictureBox1->TabIndex = 9;
-			this->pictureBox1->TabStop = false;
-			// 
 			// MyForm
 			// 
 			this->AutoScaleDimensions = System::Drawing::SizeF(8, 16);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
-			this->ClientSize = System::Drawing::Size(899, 253);
-			this->Controls->Add(this->pictureBox1);
+			this->ClientSize = System::Drawing::Size(length, height);
 			this->Controls->Add(this->button2);
 			this->Controls->Add(this->button1);
 			this->Controls->Add(this->label7);
@@ -184,7 +176,6 @@ namespace RandomNumberGenerator {
 			this->Controls->Add(this->label1);
 			this->Name = L"MyForm";
 			this->Text = L"Random Number Generator";
-			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox1))->EndInit();
 			this->ResumeLayout(false);
 			this->PerformLayout();
 
