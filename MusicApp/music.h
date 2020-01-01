0bc85c6e0a2201cfafd2b4fccaf4b23d79faa910
brlnoble <51976754+brlnoble@users.noreply.github.com>
2020-01-01 15:54:22 -0500

    Create music.h

---
+++ MusicApp/music.h
@@ -0,0 +1,11 @@
+#pragma once
+#include<iostream>
+
+using namespace std;
+
+class music {
+	string sName; //song title
+	string sArtist; //song artist
+	string sPic; //directory of picture
+	int dur; //duration of song in seconds
+};
