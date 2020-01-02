ba08834b2a9284aef344e63a5a6d254f01ae9ef1
brlnoble <51976754+brlnoble@users.noreply.github.com>
2020-01-01 19:03:45 -0500

    Update music.h

--- MusicApp/music.h
+++ MusicApp/music.h
@@ -1,11 +1,21 @@
 #pragma once
 #include<iostream>
+#include<string>
 
 using namespace std;
+using namespace System;
 
 class music {
-	string sName; //song title
+public:
+	string sName;
 	string sArtist; //song artist
 	string sPic; //directory of picture
 	int dur; //duration of song in seconds
+};
+
+void chgSong(Windows::Forms::Label^ nameLabel, Windows::Forms::Label^ artistLabel, music song) {
+	String^ name = gcnew String(song.sName.c_str()); //converts to proper string type
+	String^ artist = gcnew String(song.sArtist.c_str());
+	nameLabel->Text = name;
+	artistLabel->Text = artist;
 };
