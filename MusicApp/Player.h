94fa8fc0cf3e75f8ce01f5bcce95f002ffc82a8d
brlnoble <51976754+brlnoble@users.noreply.github.com>
2020-01-01 19:03:19 -0500

    Update Player.h

--- MusicApp/Player.h
+++ MusicApp/Player.h
@@ -1,6 +1,11 @@
 #pragma once
 #include"music.h"
 
+const int numSongs = 4;
+int curSong = -1;
+music songs[numSongs]; //array of music objects
+
+
 namespace MusicApp {
 
 	using namespace System;
@@ -35,9 +40,12 @@ namespace MusicApp {
 				delete components;
 			}
 		}
-	//define the form components and names
+		//define the form components and names
+	private: System::Windows::Forms::Button^  nextButton;
+	private: System::Windows::Forms::Button^  prevButton;
 	private: System::Windows::Forms::Button^  playButton;
 	private: System::Windows::Forms::Label^  songTitle;
+	private: System::Windows::Forms::Label^  songArtist;
 	private: System::Windows::Forms::PictureBox^  SongImage;
 	protected:
 
@@ -56,36 +64,83 @@ namespace MusicApp {
 		/// </summary>
 		void InitializeComponent(void)
 		{
+			// ------------------------------------------------------------------------------
 			int width = 400;
 			int height = 600;
 			int numS = 4; //number of songs
-			music songs[4]; //array of music objects
-			
 
+			songs[0].sName = "Selling Flowers";
+			songs[0].sArtist = "Slow Hollows";
+			songs[1].sName = "Everything Now";
+			songs[1].sArtist = "Arcade Fire";
+			songs[2].sName = "Shiver";
+			songs[2].sArtist = "Lucy Rose";
+			songs[3].sName = "Bloom";
+			songs[3].sArtist = "Paper Kites";
+
+			// ------------------------------------------------------------------------------
+			this->nextButton = (gcnew System::Windows::Forms::Button());
+			this->prevButton = (gcnew System::Windows::Forms::Button());
 			this->playButton = (gcnew System::Windows::Forms::Button());
 			this->songTitle = (gcnew System::Windows::Forms::Label());
+			this->songArtist = (gcnew System::Windows::Forms::Label());
 			this->SongImage = (gcnew System::Windows::Forms::PictureBox());
 			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->SongImage))->BeginInit();
 			this->SuspendLayout();
 
 			// 
-			// playButton
+			// nextButton
+			//
+			int pbSize = 50;
+			this->nextButton->Anchor = static_cast<System::Windows::Forms::AnchorStyles>((System::Windows::Forms::AnchorStyles::Top | System::Windows::Forms::AnchorStyles::Right));
+			this->nextButton->Location = System::Drawing::Point(3*width/4 -pbSize/2, 3 * height / 4);
+			this->nextButton->Name = L"nextButton";
+			this->nextButton->Size = System::Drawing::Size(pbSize, pbSize);
+			this->nextButton->TabIndex = 1;
+			this->nextButton->Text = L" >> ";
+			this->nextButton->UseVisualStyleBackColor = true;
+			this->nextButton->Click += gcnew System::EventHandler(this, &Player::nextClick);
 			// 
-			this->playButton->Location = System::Drawing::Point(168, 420);
+			// prevButton
+			//
+			this->prevButton->Anchor = static_cast<System::Windows::Forms::AnchorStyles>((System::Windows::Forms::AnchorStyles::Top | System::Windows::Forms::AnchorStyles::Right));
+			this->prevButton->Location = System::Drawing::Point(width/4 - pbSize/2, 3 * height / 4);
+			this->prevButton->Name = L"prevButton";
+			this->prevButton->Size = System::Drawing::Size(pbSize, pbSize);
+			this->prevButton->TabIndex = 1;
+			this->prevButton->Text = L" << ";
+			this->prevButton->UseVisualStyleBackColor = true;
+			this->prevButton->Click += gcnew System::EventHandler(this, &Player::prevClick);
+			this->prevButton->Enabled = false;
+			// 
+			// playButton
+			//
+			this->playButton->Anchor = static_cast<System::Windows::Forms::AnchorStyles>((System::Windows::Forms::AnchorStyles::Top | System::Windows::Forms::AnchorStyles::Right));
+			this->playButton->Location = System::Drawing::Point((width - pbSize) / 2, 3 * height / 4);
 			this->playButton->Name = L"playButton";
-			this->playButton->Size = System::Drawing::Size(75, 23);
+			this->playButton->Size = System::Drawing::Size(pbSize, pbSize);
 			this->playButton->TabIndex = 1;
-			this->playButton->Text = L"Play";
+			this->playButton->Text = L" || ";
 			this->playButton->UseVisualStyleBackColor = true;
+			this->playButton->Click += gcnew System::EventHandler(this, &Player::playClick);
 			// 
 			// songTitle
 			// 
-			this->songTitle->AutoSize = true;
-			this->songTitle->Location = System::Drawing::Point(88, 325);
+			this->songTitle->Location = System::Drawing::Point(0, height / 2); //position
 			this->songTitle->Name = L"songTitle";
-			this->songTitle->Size = System::Drawing::Size(35, 17);
+			this->songTitle->Size = System::Drawing::Size(width, 20); //make entire width of form to centre properly
 			this->songTitle->TabIndex = 2;
-			this->songTitle->Text = L"Title";
+			this->songTitle->Text = L"Title goes here I suppose";
+			this->songTitle->TextAlign = System::Drawing::ContentAlignment::MiddleCenter;
+			// 
+			// songArtist
+			// 
+			this->songArtist->Location = System::Drawing::Point(0, height / 2 + 30); //position
+			this->songArtist->Name = L"songArtist";
+			this->songArtist->Size = System::Drawing::Size(width, 20); //make entire width of form to centre properly
+			this->songArtist->TabIndex = 2;
+			this->songArtist->Text = L"Artist goes here I suppose";
+			this->songArtist->TextAlign = System::Drawing::ContentAlignment::MiddleCenter;
 			// 
 			// SongImage
 			// 
@@ -99,8 +154,11 @@ namespace MusicApp {
 			// 
 			this->AutoScaleDimensions = System::Drawing::SizeF(8, 16);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
-			this->ClientSize = System::Drawing::Size(400, 600);
+			this->ClientSize = System::Drawing::Size(width, height);
 			this->Controls->Add(this->songTitle);
+			this->Controls->Add(this->songArtist);
+			this->Controls->Add(this->nextButton);
+			this->Controls->Add(this->prevButton);
 			this->Controls->Add(this->playButton);
 			this->Controls->Add(this->SongImage);
 			this->FormBorderStyle = System::Windows::Forms::FormBorderStyle::FixedSingle; //prevents user from scaling window
@@ -112,5 +170,25 @@ namespace MusicApp {
 
 		}
 #pragma endregion
+	private: System::Void nextClick(System::Object^  sender, System::EventArgs^  e) {
+		curSong++;
+		chgSong(songTitle, songArtist, songs[curSong]);
+		if(curSong > 0)
+			prevButton->Enabled = true;
+		if (curSong >= numSongs - 1) {
+			nextButton->Enabled = false;
+			return;
+		}
+	}
+	private: System::Void prevClick(System::Object^  sender, System::EventArgs^  e) {
+		curSong--;
+		if (curSong == 0)
+			prevButton->Enabled = false;
+		chgSong(songTitle, songArtist, songs[curSong]);
+		nextButton->Enabled = true;
+	}
+	private: System::Void playClick(System::Object^  sender, System::EventArgs^  e) {
+		//
+	}
 	};
 }
